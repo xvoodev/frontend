b73416d2425dcdbd6c942cce8f32c940d99a1ac2
Yuri <whiz.y.wpt@list.ru>
2018-05-03 12:21:02 +0300

    New start

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const server = http.createServer(function (request, response) {
         response.end(text);
     }
 });
+console.log('log= ', process.env.PORT);
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log('Server is running..');
