09d299c1e09ed1db226d23fdc7de8fa3a830d267
Юрий Кирель <whiz.y.wpt@list.ru>
2018-05-03 16:37:06 +0300

    Stage1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const server = http.createServer(function (request, response) {
     console.log(request.method, request.url);
-    if (request.url == 'style.css')
+    if (request.url == '/style.css')
     {
         const css = fs.readFileSync('style.css', 'utf8');
         response.end(css);
@@ -16,7 +16,8 @@ const server = http.createServer(function (request, response) {
         response.end(text);
     }
 });
-console.log(process.env.PORT);
+
+console.log('log= ', process.env.PORT);
 
 server.listen(process.env.PORT || 3000);
 console.log('Server is running..');
