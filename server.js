f094bb4e549232178d72f6f690c38d76ee502a29
Yuri <whiz.y.wpt@list.ru>
2018-05-03 04:08:26 +0300

    'Initial'

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (request, response) {
+    console.log(request.method, request.url);
+    if (request.url == '/style.css')
+    {
+        const css = fs.readFileSync('style.css', 'utf8');
+        response.end(css);
+    }
+    else
+    {
+        const text = fs.readFileSync('index.html', 'utf8');
+        response.end(text);
+    }
+});
+
+server.listen(3000);
+console.log('Server is running..');
